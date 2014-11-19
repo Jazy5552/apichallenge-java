68237358a928933e85dba0a56ba546f4d7688517
Jazy Llerena <jazy555@hotmail.com>
2014-11-18 19:01:15 -0500

    Cleaned up some code before starting stage 2

---
+++ README.txt
@@ -0,0 +1,6 @@
+Created by Jassiel (Jazy) Llerena for the Code2040 ApiChallenge
+Made using eclipse and ant to create jar file.
+Also used java JSON library.
+
+Opensource software.
+
--- src/jazy/apichallenge/Main.java
+++ src/jazy/apichallenge/Main.java
@@ -7,59 +7,32 @@ import org.json.*;
 public class Main {
 
 	public static void main(String[] args) {
-		OutputStreamWriter osw;
-		BufferedReader br;
-		URL site;
-		URLConnection con;
-		JSONObject send;
-		JSONObject reci;
-		String token;
 		try
 		{
+			BufferedReader br;
+			JSONObject send;
+			JSONObject reci;
+			String token;
+			
+			//Prestage (Send info and get token)
 			send = new JSONObject();
 			send.put("email","jazy555@hotmail.com");
 			send.put("github","https://github.com/Jazy5552");
-			site = new URL("http://challenge.code2040.org/api/register");
-			con = site.openConnection();
-			con.setDoOutput(true);
-			con.setDoInput(true);
 			
-			con.connect();
-			
-			osw = new OutputStreamWriter(con.getOutputStream());
-			osw.write(send.toString());
-			osw.flush();
-			osw.close();
-			
-			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			br = postStringToSite(send.toString(), "http://challenge.code2040.org/api/register");
 			reci = new JSONObject(br.readLine());
 			br.close();
 			
 			token = reci.getString("result");
-			
 			System.out.println("Token: " + token);
 			
-			//Stage 1
+			//Stage 1 (Reverse string)
 			send = new JSONObject();
 			send.put("token", reci.getString("result"));
 			
-			site = new URL("http://challenge.code2040.org/api/getstring");
-			con = site.openConnection();
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			
-			con.connect();
-			
-			osw = new OutputStreamWriter(con.getOutputStream());
-			osw.write(send.toString());
-			osw.flush();
-			osw.close();
-			
-			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			br = postStringToSite(send.toString(), "http://challenge.code2040.org/api/getstring");
 			reci = new JSONObject(br.readLine());
 			br.close();
-			
-			System.out.println(reci.toString());
 			
 			String tmp = reci.getString("result");
 			tmp = new StringBuilder(tmp).reverse().toString();
@@ -68,31 +41,38 @@ public class Main {
 			send.put("token", token);
 			send.put("string", tmp);
 			
+			System.out.println(reci.toString());
 			System.out.println(send.toString());
 			
-			site = new URL("http://challenge.code2040.org/api/validatestring");
-			con = site.openConnection();
-			con.setDoInput(true);
-			con.setDoOutput(true);
-			
-			con.connect();
-			
-			osw = new OutputStreamWriter(con.getOutputStream());
-			osw.write(send.toString());
-			osw.flush();
-			osw.close();
-			
-			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			br = postStringToSite(send.toString(), "http://challenge.code2040.org/api/validatestring");
+
 			System.out.println(br.readLine());
 			br.close();
+			//END OF STAGE 1
 			
 		}
 		catch(Exception e)
 		{
-			
+			System.out.println("Error, program halted!");
 			System.out.println(e.toString());
 		}
 		
+	}
+	
+	public static BufferedReader postStringToSite(String msg, String site) throws Exception
+	{
+		URLConnection con = new URL(site).openConnection();
+		con.setDoInput(true);
+		con.setDoOutput(true);
+		
+		con.connect();
+		
+		OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream());
+		osw.write(msg);
+		osw.flush();
+		osw.close();
+		
+		return new BufferedReader(new InputStreamReader(con.getInputStream()));
 	}
 
 }
